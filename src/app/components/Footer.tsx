189606b0dc3ed4f332480a064bec30d0190cc751
Luciano Lasala <luciano.lasala12@hotmail.com>
2026-06-15 20:18:05 -0300

    add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-16 flex flex-col md:flex-row md:items-start bg-gray-800 p-8 rounded-lg gap-4 justify-between">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={36}
+          height={36}
+          className="W-6 h-6 md:w-9 md:h-9"
+        />
+        <p className="text-sm text-gray-400">© 2026 Full course.</p>
+        <p className="text-sm text-gray-400">All rights reserved.</p>
+      </div>
+
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-sm text-white">Links</p>
+        <Link href="./home" className="text-sm text-gray-400">
+          Home page
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Contact
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Terms of service
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Privacy policy
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-sm text-white">Products</p>
+        <Link href="/" className="text-sm text-gray-400">
+          All products
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          New products
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          New arrivals
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Best sellers
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Sales
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-sm text-white">Company</p>
+        <Link href="/" className="text-sm text-gray-400">
+          About
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Contact
+        </Link>
+        <Link href="/" className="text-sm text-gray-400">
+          Blog
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
